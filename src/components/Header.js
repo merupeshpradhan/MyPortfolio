c14f17d5ef202bd9ed1857278680dcd17c8e1037
Rupesh pradhan <merupeshpradhan@gmail.com>
2023-09-23 23:34:31 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Logo from "../assets/mylogo.png";
+
+const Header = () => {
+  return (
+    <header className=" py-2">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          <a href="#">
+            <img src={Logo} alt="" className="w-9"/>
+          </a>
+          <button className="btn btn-sm">Work with me</button>
+        </div>
+      </div>
+    </header>
+  )
+};
+
+export default Header;
