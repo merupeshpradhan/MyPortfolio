a9f9be6d2afa064f6edd93c8372a1d2ea239e3b2
Rupesh pradhan <merupeshpradhan@gmail.com>
2023-09-25 11:01:35 +0530

    update pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-// components
 import Banner from './components/Banner';
 import Header from './components/Header';
 import Nav from './components/Nav';
@@ -8,7 +6,8 @@ import Services from './components/Services';
 import Work from './components/Work';
 import Contact from './components/Contact';
 
-const App = () => {
+function App() {
+
   return (
     <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
       <Header />
@@ -20,7 +19,7 @@ const App = () => {
       <Contact />
       {/* <div className='h-[4000px]'></div> */}
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
